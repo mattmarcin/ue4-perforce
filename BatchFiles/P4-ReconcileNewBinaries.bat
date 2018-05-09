6606f8eefe1de49eb988511f51ed67075ca84fe5
mattmarcin <m@mattmarcin.com>
2018-05-08 18:20:05 -0700

    batchfile
    helper batch file

---
+++ BatchFiles/P4-ReconcileNewBinaries.bat
@@ -0,0 +1,15 @@
+p4 reconcile GameProject\Binaries\....dll
+p4 reconcile GameProject\Binaries\....pdb
+p4 reconcile GameProject\Binaries\....exe
+p4 reconcile GameProject\Binaries\....self
+p4 reconcile GameProject\Binaries\....target
+p4 reconcile GameProject\Plugins\...\Binaries\....dll
+p4 reconcile GameProject\Plugins\...\Binaries\....pdb
+p4 reconcile Engine\Binaries\....dll
+p4 reconcile Engine\Binaries\....pdb
+p4 reconcile Engine\Binaries\....exe
+p4 reconcile Engine\Plugins\...\Binaries\....dll
+p4 reconcile Engine\Plugins\...\Binaries\....pdb
+
+echo ####### Changed Binaries Added to Changelist ######
+PAUSE
